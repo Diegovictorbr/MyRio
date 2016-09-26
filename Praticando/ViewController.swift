2668c95830d149fe852a53c002062e995ba4d05a
md10 <md10@sj208-10.local>
2016-09-26 18:52:27 -0300

    Initial Commit

---
+++ Praticando/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Praticando
+//
+//  Created by md10 on 9/26/16.
+//  Copyright © 2016 infnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
